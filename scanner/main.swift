8cd1c0325739f2d8753aabed722e664fa066aa36
David Gunzinger <david.gunzinger@smoca.ch>
2024-01-04 14:42:25 +0100

    works fast

--- scanner/main.swift
+++ scanner/main.swift
@@ -23,94 +23,108 @@ class Statistic {
 print("Hello, World!")
 let path = "/Users/pfy/Devel/1brc/measurements.txt"
 let bufferSize = 1024 * 1024 * 16
-let newline = "\n".data(using: .utf8)!
-let semicolon = ";".data(using: .utf8)!
-var byCity = [String: Statistic]()
+let newline = "\n".data(using: .utf8)![0]
+let semicolon = ";".data(using: .utf8)![0]
+let zero = "0".data(using: .utf8)![0]
+let point = ".".data(using: .utf8)![0]
+let minus = "-".data(using: .utf8)![0]
 
-guard let handle = try? FileHandle(forReadingFrom: URL(fileURLWithPath: path)) else {
-    print("can't open file")
-    exit(1)
-}
-var leftover = Data()
-while true {
-    guard var data = try handle.read(upToCount: bufferSize) else {
-        break
+var byCity = [[UInt8]: Statistic]()
+var byCityLock = NSRecursiveLock()
+
+
+let data = try! Data(contentsOf: URL(fileURLWithPath: path), options: .alwaysMapped)
+let numberOfCores = ProcessInfo.processInfo.activeProcessorCount
+
+var datas = [] as [Data]
+
+var start = 0
+for i in 0..<numberOfCores {
+    var end  =  data.count / numberOfCores * (i + 1)
+    while (data[end] != newline) {
+        end += 1
     }
-    data = leftover + data
-    var searchRange = 0..<data.count
-    var newlineRange = data.range(of: newline, in: searchRange)
-    while newlineRange != nil {
-        let semicolonRange = data.range(of: semicolon, in: searchRange.lowerBound..<newlineRange!.lowerBound)
-        guard let semicolonRange = semicolonRange else {
-            print("no semicolon in line")
-            exit(1)
-        }
+    datas.append(data.subdata(in: start..<end))
+    start = end + 1
+}
+
+let operationQueue = OperationQueue()
+
+for subdata in datas {
+    operationQueue.addOperation {
+        var byCityThreaded = [[UInt8]: Statistic]()
         
-        let city = String(data: data.subdata(in: searchRange.lowerBound..<semicolonRange.lowerBound), encoding: .utf8)!
-        let value = Float(String(data: data.subdata(in: semicolonRange.upperBound..<newlineRange!.lowerBound), encoding: .ascii)!)!
-        if let statistic = byCity[city] {
-            statistic.max = max(statistic.max, value);
-            statistic.min = max(statistic.min, value);
-            statistic.count += 1
-            statistic.sum += value
-        } else {
-            byCity[city] = Statistic(min: value, max: value, count: 1, sum: value)
+        subdata.withUnsafeBytes { bytes in
+            var iterator = bytes.makeIterator()
+        while true {
+            var cityNameBytes = [] as [UInt8]
+            while let byte = iterator.next()   {
+                if byte == semicolon {
+                    break
+                }
+                cityNameBytes.append(byte)
+            }
+            
+           // var cityNameString = String(bytes: cityNameBytes, encoding: .utf8)
+            var cityValue = 0 as Int
+            var valueSign = 1
+            if let byte = iterator.next() {
+                if byte == minus {
+                    valueSign = -1;
+                } else {
+                    cityValue = Int(byte - zero)
+                }
+            } else {
+                break
+            }
+            while let byte = iterator.next()  {
+                if byte == newline {
+                    break;
+                }
+                
+                if (byte != point) {
+                    let val = byte - zero
+                    cityValue = cityValue * 10 + Int(val)
+                }
+            }
+            
+            let value = Float(cityValue * valueSign) / 10
+            let cityName = cityNameBytes
+            //byCityLock.withLock {
+            if let statistic = byCityThreaded[cityName] {
+                statistic.max = max(statistic.max, value);
+                statistic.min = min(statistic.min, value);
+                statistic.count += 1
+                statistic.sum += value
+            } else {
+                byCityThreaded[cityName] = Statistic(min: value, max: value, count: 1, sum: value)
+            }
         }
-        searchRange = newlineRange!.upperBound..<data.count
-        newlineRange = data.range(of: newline, in: searchRange)
-    }
-    leftover = data.subdata(in: searchRange)
-//    let parts = data.split(separator: newline)
-    if  data.isEmpty {
-        break;
+            byCityLock.withLock {
+                for (cityName, value) in byCityThreaded {
+                    if let statistic = byCity[cityName] {
+                        statistic.max = max(statistic.max, value.max)
+                        statistic.min = min(statistic.max, value.max)
+                        statistic.count = statistic.count + value.count
+                        statistic.sum = statistic.sum + value.sum
+                    } else {
+                        byCity[cityName] = value
+                    }
+                }
+            }
+        }
     }
 }
-/*
-for try await line in handle.bytes.lines {
-    let parts = line.split(separator: ";")
-    let city = String(parts[0])
-    let value = Float(parts[1])!
-    if let statistic = byCity[city] {
-        statistic.max = max(statistic.max, value);
-        statistic.min = max(statistic.min, value);
-        statistic.count += 1
-        statistic.sum += value
-    } else {
-        byCity[city] = Statistic(min: value, max: value, count: 1, sum: value)
-    }
-}*/
 
+operationQueue.waitUntilAllOperationsAreFinished()
 
-
-/*guard let data = try? Data(contentsOf: URL(filePath: path), options: .alwaysMapped) else {
-    print("can't map data")
-    exit(1)
-}
-let newlineData = "\n".data(using: .utf8)![0]
-let semicolonData = ";".data(using: .utf8)![0]
-let ranges = data.ranges(of: [newlineData, semicolonData])
-exit(0)
-*/
-/*
-for line in reader {
-    let parts = line.split(separator: ";")
-    let city = String(parts[0])
-    let value = Float(parts[1].replacing("\n", with: ""))!
-    if var statistic = byCity[city] {
-        statistic.max = max(statistic.max, value);
-        statistic.min = max(statistic.min, value);
-        statistic.count += 1
-        statistic.sum += value
-    } else {
-        byCity[city] = Statistic(min: value, max: value, count: 1, sum: value)
-    }
-    //print(">" + line.trimmingCharacters(in: .whitespacesAndNewlines))
-}*/
-let output = byCity.keys.sorted().map{ key in
-    let statistics = byCity[key]!
-    
-    
-    return "\(key)=\(statistics.min)/\(statistics.sum / Float(statistics.count))/\(statistics.max)"
-}.joined(separator: ",")
+print("All tasks completed")
+let output = byCity.keys.map({ data in
+    return (String(bytes: data, encoding: .utf8)!, data)
+}).sorted(by: { a, b in
+    return a.0 < b.0
+}).map{ key in
+    let statistics = byCity[key.1]!
+    return  String(format: "%@=%.1f/%.1f/%.1f", key.0, statistics.min, statistics.sum / Float(statistics.count), statistics.max)
+}.joined(separator: ", ")
 print("{\(output)}")
-exit(0)
